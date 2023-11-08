68362d36010e7ba269ce26a2c35778de19a84784
masha <mativaclub@gmail.com>
2023-11-08 09:20:30 +0200

    Initial commit

---
+++ src/main/java/pro/sky/diplom/DiplomApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomApplication.class, args);
+	}
+
+}
